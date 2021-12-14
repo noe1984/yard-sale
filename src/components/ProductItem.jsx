2bc1b7a8e4c8d6a52c3a2500b3db44679c4ffefc
Noe Gonzalez <palmas.inter@gmail.com>
2021-12-13 23:42:47 -0800

    Header - React Context

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import '@styles/ProductItem.scss';
 import AddToCartImage from '@icons/bt_add_to_cart.svg';
 import AppContext from '../context/AppContext'
